b97ceef03ab652f9214201bca09e5c76d91d692e
WhitePencil <skripkin679@gmail.com>
2025-12-19 15:28:13 +0400

    Решил задачу с подсчетом всех правильных скобочных последовательностей

--- src/main/java/problems/Main.java
+++ src/main/java/problems/Main.java
@@ -5,62 +5,26 @@ import java.util.*;
 
 public class Main {
 	
-	public static int[] getEnoughDays(int[] sum, int bikeCost) {
-		int firstDay = getEnoughDay(sum, bikeCost);
-		int secondDay = getEnoughDay(sum, bikeCost * 2);
-
-		return new int[] {firstDay, secondDay};		
+	public static void printAllSeq(int n) {
+		printSeq(n, 0, 0, "");
 	}
 	
-//	public static int getEnoughDay(int[] sum, int left, int right, int minCost) {
-//		int mid = left + ((right - left) / 2);
-//		
-//		
-//		if(mid - 1 >= left) {
-//			if(sum[mid - 1] >= minCost) {
-//				return getEnoughDay(sum, left, mid, minCost);
-//			}
-//		}
-//		
-//		if(sum[mid] >= minCost) {
-//			return mid + 1;
-//		}
-//		
-//		if(mid + 1 <= right) {
-//			return getEnoughDay(sum, mid + 1, right, minCost);
-//		}
-//		
-//		return -1;
-//	}
-	
-	public static int getEnoughDay(int[] sum, int minCost) {
-		int left = 0;
-		int right = sum.length - 1;
-		int answer = -1;
-		
-		
-		while(left <= right) {
-			int mid = left + ((right - left) / 2);
-			
-			if(sum[mid] >= minCost) {
-				answer = mid;
-				right = mid - 1;
-			}
-			else {
-				left = mid + 1;
-			}
-			
-			
-			
+	public static void printSeq(int n, int open, int close, String prefix) {
+		if(open == n && open == close) {
+			System.out.println(prefix);
 		}
-		return answer == -1 ? -1 : answer + 1;
+		
+	    if(open < n) {
+	    	printSeq(n, open + 1, close, prefix + "(");
+	    }
+	    if(close < open) {
+	    	printSeq(n, open, close + 1, prefix + ")");
+	    }
+		
 	}
 	
 	
 	public static void main(String[] args) {	
-		int[] sum = {1, 2, 4, 4, 4, 4};
-		int bikeCost = 10;
-		int[] days = getEnoughDays(sum, bikeCost);
-		System.out.println(Arrays.toString(days));
+		printAllSeq(3);
 	}
 }
