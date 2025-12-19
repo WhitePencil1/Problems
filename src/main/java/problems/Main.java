d718808aa5158283d4e10d38dc976be579388f62
WhitePencil <skripkin679@gmail.com>
2025-12-19 14:37:47 +0400

    Решил задачу про два велосипеда

--- src/main/java/problems/Main.java
+++ src/main/java/problems/Main.java
@@ -5,50 +5,62 @@ import java.util.*;
 
 public class Main {
 	
-	public static int shiftUp(int[] heap, int idx) {
-		if(idx == 1) {
-			return idx;
-		}
-		int parent = idx / 2;
-		if(heap[parent] < heap[idx]) {
-			int temp = heap[idx];
-			heap[idx] = heap[parent];
-			heap[parent] = temp;
-			return shiftUp(heap, parent);
-		}
-		else {
-			return idx;
-		}
+	public static int[] getEnoughDays(int[] sum, int bikeCost) {
+		int firstDay = getEnoughDay(sum, bikeCost);
+		int secondDay = getEnoughDay(sum, bikeCost * 2);
+
+		return new int[] {firstDay, secondDay};		
 	}
 	
+//	public static int getEnoughDay(int[] sum, int left, int right, int minCost) {
+//		int mid = left + ((right - left) / 2);
+//		
+//		
+//		if(mid - 1 >= left) {
+//			if(sum[mid - 1] >= minCost) {
+//				return getEnoughDay(sum, left, mid, minCost);
+//			}
+//		}
+//		
+//		if(sum[mid] >= minCost) {
+//			return mid + 1;
+//		}
+//		
+//		if(mid + 1 <= right) {
+//			return getEnoughDay(sum, mid + 1, right, minCost);
+//		}
+//		
+//		return -1;
+//	}
 	
-	public static int shiftDown(int[] heap, int idx) {
-		int left = idx * 2;
-		int right = idx * 2 + 1;
+	public static int getEnoughDay(int[] sum, int minCost) {
+		int left = 0;
+		int right = sum.length - 1;
+		int answer = -1;
 		
-		if(heap.length - 1 < left) return idx;
-		int largestIndex = left;
-		if(right <= heap.length - 1 && heap[left] < heap[right]) {
-			largestIndex = right;
+		
+		while(left <= right) {
+			int mid = left + ((right - left) / 2);
+			
+			if(sum[mid] >= minCost) {
+				answer = mid;
+				right = mid - 1;
+			}
+			else {
+				left = mid + 1;
+			}
+			
+			
+			
 		}
-
-		if(heap[idx] >= heap[largestIndex]) {
-			return idx;
-		}
-		
-		int temp = heap[idx];
-		heap[idx] = heap[largestIndex];
-		heap[largestIndex] = temp;
-		
-		return shiftDown(heap, largestIndex);
+		return answer == -1 ? -1 : answer + 1;
 	}
 	
 	
 	public static void main(String[] args) {	
-		int[] heapUp = {0, 12, 6, 8, 3, 15, 7};
-		System.out.println(shiftUp(heapUp, 5));
-		
-		int[] heapDown = {0, 12, 1, 8, 3, 4, 7};
-		System.out.println(shiftDown(heapDown, 2));
+		int[] sum = {1, 2, 4, 4, 4, 4};
+		int bikeCost = 10;
+		int[] days = getEnoughDays(sum, bikeCost);
+		System.out.println(Arrays.toString(days));
 	}
 }
