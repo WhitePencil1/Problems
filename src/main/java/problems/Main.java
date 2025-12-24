85eff86b51019c0afc606ad339bf3696b36807ec
WhitePencil <skripkin679@gmail.com>
2025-12-24 17:06:57 +0400

    BFS с поиском кратчайшего расстояния

--- src/main/java/problems/Main.java
+++ src/main/java/problems/Main.java
@@ -7,42 +7,70 @@ import java.util.*;
 public class Main {
 	
 	
-	public static List<List<Integer>> getComponents(List<List<Integer>> list) {
+	public static String getOrderSeq(List<List<Integer>> edges, int start) {
+		Colors[] colors = new Colors[edges.size()];
+		Arrays.fill(colors, Colors.WHITE);
 		
-		int[] colors = new int[list.size()];
-		Arrays.fill(colors, 0);
-		int colorsCounter = 0;
+		StringBuilder builder = new StringBuilder();
 		
-		Deque<Integer> stack = new ArrayDeque<>();
-		for(int i = 1; i < list.size(); i++) {
-			if(colors[i] != 0) {
-				continue;
-			}
-			colorsCounter++;
-			stack.push(i);
-			while(!stack.isEmpty()) {
-				int v = stack.pop();
-				if(colors[v] == 0) {
-					colors[v] = colorsCounter;
-					for(var child : list.get(v)) {
-						if(colors[child] == 0) {
-							stack.push(child);
-						}
-					}
+		Queue<Integer> planned = new ArrayDeque<>();
+		planned.add(start);
+		colors[start] = Colors.GRAY;
+		
+		while(!planned.isEmpty()) {
+			int v = planned.poll();
+			builder.append(v).append(" ");
+			for(var child : edges.get(v)) {
+				if(colors[child] == Colors.WHITE) {
+					colors[child] = Colors.GRAY;
+					planned.add(child);
 				}
 			}
+			colors[v] = Colors.BLACK;
 		}
-
-		List<List<Integer>> components = new ArrayList<>();
-		for(int i = 0; i <= colorsCounter; i++) {
-			components.add(new ArrayList<>());
-		}
-		for(int i = 1; i < colors.length; i++) {
-			components.get(colors[i]).add(i);
-		}
-		return components;
+		return builder.toString();
 	}
 	
+	
+	public static Deque<Integer> getMinDist(List<List<Integer>> edges, int start, int end) {
+		Colors[] colors = new Colors[edges.size()];
+		Arrays.fill(colors, Colors.WHITE);
+		
+		Integer[] previous = new Integer[edges.size()];
+		Integer[] distance = new Integer[edges.size()];
+		
+		Queue<Integer> planned = new ArrayDeque<>();
+		planned.add(start);
+		colors[start] = Colors.GRAY;
+		distance[start] = 0;
+		
+		while(!planned.isEmpty()) {
+			int v = planned.poll();
+			for(var child : edges.get(v)) {
+				if(colors[child] == Colors.WHITE) {
+					
+					distance[child] = distance[v] + 1;
+					previous[child] = v;
+					
+					colors[child] = Colors.GRAY;
+					planned.add(child);
+				}
+			}
+			colors[v] = Colors.BLACK;
+		}
+		
+		if(distance[end] == null) {
+			return new ArrayDeque<>();
+		}
+		
+		Deque<Integer> path = new ArrayDeque<>();
+		Integer cur = end;
+		while(cur != null) {
+			path.addFirst(cur);
+			cur = previous[cur];
+		}
+		return path;
+	}
 	
 	public static List<List<Integer>> getAdjacencyList(Edge[] edges, int vertexNum) {
 		List<List<Integer>> resultList = new ArrayList<>();
@@ -60,17 +88,20 @@ public class Main {
 		}
 
 		for(int i = 0; i < resultList.size(); i++) {
-			Collections.sort(resultList.get(i), (a, b) -> b - a);
+			Collections.sort(resultList.get(i), (a, b) -> a - b);
 		}
 		return resultList;
 	}
 	
 	
 	public record Edge(int start, int end) {};
+	public enum Colors {WHITE, BLACK, GRAY};
 	public static void main(String[] args) {	
-		int vertexNum = 4;
-		List<List<Integer>> adjList = getAdjacencyList(new Edge[] {new Edge(2, 3), new Edge(2, 1), new Edge(4, 3)}, vertexNum);
-		List<List<Integer>> components = getComponents(adjList);
-		System.out.println(components);
+		int vertexNum = 5;
+		int startV = 1;
+		int endV = 5;
+		List<List<Integer>> adjList = getAdjacencyList(new Edge[] {new Edge(1, 2), new Edge(1, 3), new Edge(2, 5), new Edge(2, 4), new Edge(3, 4), new Edge(4, 5)}, vertexNum);
+		System.out.println(getMinDist(adjList, startV, endV));
+
 	}
 }
