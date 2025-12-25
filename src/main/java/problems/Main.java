118e4cfe7a99df5181c1d3507c41ae2dd65be5f9
WhitePencil <skripkin679@gmail.com>
2025-12-25 14:11:43 +0400

    Алгоритм Дейкстры

--- src/main/java/problems/Main.java
+++ src/main/java/problems/Main.java
@@ -7,101 +7,84 @@ import java.util.*;
 public class Main {
 	
 	
-	public static String getOrderSeq(List<List<Integer>> edges, int start) {
-		Colors[] colors = new Colors[edges.size()];
-		Arrays.fill(colors, Colors.WHITE);
+	public static List<Integer> dijkstra(List<List<Edge>> graph, int start, int end) {
 		
-		StringBuilder builder = new StringBuilder();
+		int n = graph.size();
+		boolean[] visited = new boolean[n];
+		int[] dist = new int[n];
+		int[] prev = new int[n];
 		
-		Queue<Integer> planned = new ArrayDeque<>();
-		planned.add(start);
-		colors[start] = Colors.GRAY;
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		Arrays.fill(prev, -1);
 		
-		while(!planned.isEmpty()) {
-			int v = planned.poll();
-			builder.append(v).append(" ");
-			for(var child : edges.get(v)) {
-				if(colors[child] == Colors.WHITE) {
-					colors[child] = Colors.GRAY;
-					planned.add(child);
+		dist[start] = 0;
+		
+		for(int i = 1; i < n; i++) {
+			
+			int v = -1;
+			for(int j = 1; j < n; j++) {
+				if(!visited[j] && (v == -1 || dist[j] < dist[v])) {
+					v = j;
 				}
 			}
-			colors[v] = Colors.BLACK;
-		}
-		return builder.toString();
-	}
-	
-	
-	public static Deque<Integer> getMinDist(List<List<Integer>> edges, int start, int end) {
-		Colors[] colors = new Colors[edges.size()];
-		Arrays.fill(colors, Colors.WHITE);
-		
-		Integer[] previous = new Integer[edges.size()];
-		Integer[] distance = new Integer[edges.size()];
-		
-		Queue<Integer> planned = new ArrayDeque<>();
-		planned.add(start);
-		colors[start] = Colors.GRAY;
-		distance[start] = 0;
-		
-		while(!planned.isEmpty()) {
-			int v = planned.poll();
-			for(var child : edges.get(v)) {
-				if(colors[child] == Colors.WHITE) {
-					
-					distance[child] = distance[v] + 1;
-					previous[child] = v;
-					
-					colors[child] = Colors.GRAY;
-					planned.add(child);
+			
+			if(dist[v] == Integer.MAX_VALUE) break;
+			
+			
+			visited[v] = true;
+			for(var e : graph.get(v)) {
+				if(dist[e.to] > dist[v] + e.w) {
+					dist[e.to] = dist[v] + e.w;
+					prev[e.to] = v;
 				}
 			}
-			colors[v] = Colors.BLACK;
 		}
 		
-		if(distance[end] == null) {
-			return new ArrayDeque<>();
-		}
 		
-		Deque<Integer> path = new ArrayDeque<>();
-		Integer cur = end;
-		while(cur != null) {
-			path.addFirst(cur);
-			cur = previous[cur];
+		List<Integer> path = new ArrayList<>();
+		
+		int curV = end;
+		
+		while(curV != -1) {
+			path.add(curV);
+			curV = prev[curV];
 		}
+		Collections.reverse(path);
+		
 		return path;
 	}
 	
-	public static List<List<Integer>> getAdjacencyList(Edge[] edges, int vertexNum) {
-		List<List<Integer>> resultList = new ArrayList<>();
-		
-		for (int i = 0; i <= vertexNum; i++) {
-			resultList.add(new ArrayList<>());
-		}
-		
-		for(int i = 0; i < edges.length; i++) {
-			int curVertex = edges[i].start;
-			int reverseVertex = edges[i].end;
-			
-			resultList.get(curVertex).add(reverseVertex);
-			resultList.get(reverseVertex).add(curVertex);
-		}
-
-		for(int i = 0; i < resultList.size(); i++) {
-			Collections.sort(resultList.get(i), (a, b) -> a - b);
-		}
-		return resultList;
-	}
 	
+//	public static List<List<Integer>> getAdjacencyList(Edge[] edges, int vertexNum) {
+//		List<List<Integer>> resultList = new ArrayList<>();
+//
+//		for (int i = 0; i <= vertexNum; i++) {
+//			resultList.add(new ArrayList<>());
+//		}
+//
+//		for(int i = 0; i < edges.length; i++) {
+//			int curVertex = edges[i].start;
+//			int reverseVertex = edges[i].end;
+//
+//			resultList.get(curVertex).add(reverseVertex);
+//			resultList.get(reverseVertex).add(curVertex);
+//		}
+//
+//		for(int i = 0; i < resultList.size(); i++) {
+//			Collections.sort(resultList.get(i), (a, b) -> b - a);
+//		}
+//		return resultList;
+//	}
 	
-	public record Edge(int start, int end) {};
-	public enum Colors {WHITE, BLACK, GRAY};
+	public record Edge(int to, int w) {};
 	public static void main(String[] args) {	
-		int vertexNum = 5;
-		int startV = 1;
-		int endV = 5;
-		List<List<Integer>> adjList = getAdjacencyList(new Edge[] {new Edge(1, 2), new Edge(1, 3), new Edge(2, 5), new Edge(2, 4), new Edge(3, 4), new Edge(4, 5)}, vertexNum);
-		System.out.println(getMinDist(adjList, startV, endV));
-
+		List<List<Edge>> graph = new ArrayList<>();
+		graph.add(List.of());
+		graph.add(List.of(new Edge(2, 2), new Edge(3, 1)));
+		graph.add(List.of(new Edge(5, 4), new Edge(4, 3)));
+		graph.add(List.of());
+		graph.add(List.of(new Edge(5, 4)));
+		graph.add(List.of());
+		System.out.println(dijkstra(graph, 1, 5));
 	}
 }
